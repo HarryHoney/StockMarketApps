893b274ff71def74963fadc70c8a2086292decf6
lamthao1995 <lamthao1995@gmail.com>
2017-08-10 18:54:00 +0900

    Add files via upload

---
+++ app/builds/generated/source/buildConfig/debug/com/example/nhatcuong1/app_chung_khoan/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.nhatcuong1.app_chung_khoan;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.nhatcuong1.app_chung_khoan";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
